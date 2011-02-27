846687c218be8f9bb8668be8deb030f716a64594
pussbb <pussbb@gmail.com>
2011-02-27 23:29:40 +0200

    add template media list for downloading

--- headers/taskdb.h
+++ headers/taskdb.h
@@ -21,6 +21,7 @@ public:
     bool page_exists(QString page);
     int page_index;
     int page_parsed;
+
     QString get_next_page();
     void set_page_parsed(int id,int val = 1 );
     int count_crawld();
